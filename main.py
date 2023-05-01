f919a5e97909b9010413676c4e9c716c707860b9
bigfatmidget <eugenezhang500@gmail.com>
2023-04-30 23:28:09 -0400

    added 'help' command

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ from discord.ext import tasks
 import asyncio
 import pathlib as pl
 
-
 with open('client.token', 'r') as f:
     token = f.readline().strip()
-
 
 client = commands.Bot(command_prefix='super ', help_command=None, intents=discord.Intents.all())
 
@@ -19,9 +17,14 @@ async def on_ready():
     print('Logged in')
 
 
-
 async def main():
     await client.start(token)
+
+
+@client.command()
+async def help(ctx):
+    await ctx.send('Sorry, I can\'t help you. Ah, that\'s life. I think <@377907768071553024> over there can though.')
+    print("Hello")
 
 
 if __name__ == '__main__':
