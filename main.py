a5480e50e0787004fb794736435b36143ec0403e
ez500 <eugenezhang500@gmail.com>
2024-10-06 22:34:35 -0400

    onguh free free

--- main.py
+++ main.py
@@ -129,6 +129,8 @@ async def rollthedice(ctx):
         message += 'Who got E? '
     message += f'{random.choice(source)} says the answer is {letters[ans - 1]}! '
     message += 'Super!'
+    if random.randint(0, 5) == 0:
+        message += 'Onguh free free!'
     await ctx.send(message)
 
 
